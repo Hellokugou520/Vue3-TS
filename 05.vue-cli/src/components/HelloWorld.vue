fea0af332b04a267030fded071473c803f4e5e60
Bpoi <1378400943@qq.com>
2022-06-11 17:12:29 +0800

    非 Prop 的 Attribute&emits

---
+++ 05.vue-cli/src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <!-- 当组件返回单个根节点时，非 prop 的 attribute 将自动添加到根节点的 attribute 中。 -->
+  <!-- 具有多个根节点的组件如果未显式绑定 $attrs，将发出运行时警告。 -->
+  <div>
+    <h1 v-bind="$attrs" @click="changeColor">{{ msg }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HelloWorld',
+  // 禁用 Attribute 继承
+  inheritAttrs: false,
+  props: {
+    msg: String
+  },
+  // emits: ['changeColor'],
+  // 对象写法：可以对参数进行验证
+  emits: {
+    changeColor(payload: true) {
+      if (payload) return true
+      else return false
+    }
+  },
+  methods: {
+    changeColor() {
+      this.$emit('changeColor', true)
+    }
+  }
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+h1 {
+  user-select: none;
+}
+.hello {
+  color: red;
+}
+</style>
---
+++ 05.vue-cli/src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <!-- 非 Prop 的 Attribute -->
+    <HelloWorld
+      :class="isRed ? 'hello' : ''"
+      msg="点击变色"
+      @changeColor="changeColor"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      isRed: false
+    }
+  },
+  methods: {
+    changeColor(value: boolean) {
+      if (value) this.isRed = !this.isRed
+    }
+  }
+})
+</script>
