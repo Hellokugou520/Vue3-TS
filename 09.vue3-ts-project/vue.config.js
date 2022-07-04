01398b035a272f90a5277258abc1bd530d3edcb6
Bpoi <1378400943@qq.com>
2022-07-04 15:41:44 +0800

    build: 集成element-plus
    配置element-plus自动导入

--- 09.vue3-ts-project/vue.config.js
+++ 09.vue3-ts-project/vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      // 配置ElementPlus的自动导入
+      require('unplugin-vue-components/webpack')({
+        resolvers: [ElementPlusResolver()]
+      }),
+      require('unplugin-auto-import/webpack')({
+        resolvers: [ElementPlusResolver()]
+      })
+    ]
+  }
 })
