406d7a96f61041cdc99108c0665cf1efc20e96a7
Bpoi <1378400943@qq.com>
2022-07-12 20:49:28 +0800

    feat: 完善动态表单
    补充动态表单的数据绑定,新增动态表单插槽

---
+++ 09.vue3-ts-project/src/components/page-search/page-search.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="page-search">
+    <dynamic-form v-bind="searchFormConfig" v-model="formData">
+      <template #header>
+        <h1 class="header">高级检索</h1>
+      </template>
+      <template #footer>
+        <div class="handle-btns">
+          <el-button icon="refresh">重置</el-button>
+          <el-button type="primary" icon="search">搜索</el-button>
+        </div>
+      </template>
+    </dynamic-form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  props: {
+    searchFormConfig: {
+      type: Object,
+      reuqired: true
+    }
+  },
+  setup() {
+    const formData = ref({
+      id: '',
+      name: '',
+      password: '',
+      sport: '',
+      createTime: ''
+    })
+
+    return {
+      formData
+    }
+  }
+})
+</script>
+
+<style lang="less" scoped>
+.header {
+  color: red;
+}
+.handle-btns {
+  text-align: right;
+  padding: 0 50px 20px 0;
+}
+</style>
