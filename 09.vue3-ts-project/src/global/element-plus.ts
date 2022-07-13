45694792c7918dd487722256dadd79b409519b5b
Bpoi <1378400943@qq.com>
2022-07-13 18:11:13 +0800

    build: element-plus国际化

--- 09.vue3-ts-project/src/global/element-plus.ts
+++ 09.vue3-ts-project/src/global/element-plus.ts
@@ -4,11 +4,18 @@
  * desc: element-plus相关注册
  */
 import { App } from 'vue'
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
 
-// 全局注册图标
 const registerEP = (app: App<Element>) => {
+  // 全局配置国际化
+  app.use(ElementPlus, {
+    locale: zhCn
+  })
+
+  // 全局注册图标
   for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
   }
--- 09.vue3-ts-project/src/global/index.ts
+++ 09.vue3-ts-project/src/global/index.ts
@@ -1,9 +1,16 @@
 import { App } from 'vue'
 import registerEP from './element-plus'
 import './style.ts'
+import { formatUtcString } from '@/utils'
 
 const globalRegister = (app: App<Element>) => {
   registerEP(app)
+
+  app.config.globalProperties.$filters = {
+    formatTime(value: string) {
+      return formatUtcString(value)
+    }
+  }
 }
 
 export default globalRegister
